959a64b7e42cfb3ad0dafe77c68ed71d78d3bbe1
Your Name <rohithreddy4342@gmail.com>
2025-07-15 11:37:58 +0530

    String ops

---
+++ src/String_opes.java
@@ -0,0 +1,13 @@
+public class String_opes {
+    public static void main(String[] args) {
+        String name = "riyar ";
+        String name2 = "sai";
+        String name3 = "SaiandRam";
+        System.out.println(name + name2);
+        System.out.println(name.charAt(3));
+        System.out.println(name2.length());
+        System.out.println(name.replace('r' , 'k'));
+        System.out.println(name3.substring(3,6) +" "+ name3);
+    }
+
+}
