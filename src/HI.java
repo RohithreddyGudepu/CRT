9d27e374bb152a1e14427da910dc1b4acfc0e808
Your Name <rohithreddy4342@gmail.com>
2025-07-15 11:38:20 +0530

    String ops

---
+++ src/HI.java
@@ -0,0 +1,15 @@
+public class HI {
+    public static void main(String[] args) {
+        String name = "Rohith";
+        System.out.println(name);
+        int a =10;
+        char FLAT = 'O';
+        long num = 123456789000L;
+        float f = 3.14f;
+        double d = 12233442042492943d;
+        byte b = 127;
+        boolean lol = true;
+        short s = 32767;
+        System.out.println(a +" "+ FLAT + num + f + d + b +" "+ s );
+    }
+}
