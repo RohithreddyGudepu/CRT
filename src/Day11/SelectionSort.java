580e083150d28d8d1e45aab2368c2dfad826fd3c
Your Name <rohithreddy4342@gmail.com>
2025-07-30 07:12:22 +0530

    Sorting

--- src/Day11/SelectionSort.java
+++ src/Day11/SelectionSort.java
@@ -2,12 +2,12 @@ package Day11;
 import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args) {
-        int arr[] = { 20,34,65,42,24,26};
-        SelectionSort(arr);
+        int[] arr = { 20,34,65,42,24,26};
+        SelectionSort1(arr);
         System.out.println("Sorted array: " + Arrays.toString(arr));
     }
 
-    public static void SelectionSort(int[] arr){
+    public static void SelectionSort1(int[] arr){
         for (int i = 0; i < arr.length-1; i++) {
             int smallest = i;
             for (int j = i+1; j <arr.length; j++) {
